73269ec685c24646375eb0c9374847093b73ef43
AnastasiiaTetiura <n.tetiura@gmail.com>
2023-03-29 22:53:12 -0700

    Home work 7

---
+++ classwork_7/empty.py
@@ -0,0 +1,10 @@
+from selenium.webdriver.common.by import By
+from classwork_7.base_page import Page
+
+
+class EmptyCart(Page):
+    CART_IS_EMPTY = (By.XPATH, "//div[@class='a-row sc-your-amazon-cart-is-empty']/h2")
+
+    def verify_empty_cart(self, expected_text):
+        self.verify_text(expected_text, *self.CART_IS_EMPTY)
+
