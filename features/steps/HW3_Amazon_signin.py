66b508c5f3d80d414cec09e31b19c35b517beeb2
AnastasiiaTetiura <n.tetiura@gmail.com>
2023-03-29 22:57:25 -0700

    home work 7 - Amazon Sign In steps

--- features/steps/HW3_Amazon_signin.py
+++ features/steps/HW3_Amazon_signin.py
@@ -1,28 +1,24 @@
+from selenium import webdriver
 from selenium.webdriver.common.by import By
 from behave import given, when, then
 from time import sleep
 
 
-#SEARCH_INPUT = (By.NAME, 'q')
-#SEARCH_SUBMIT = (By.NAME, 'btnK')
-
-
 @given('Open Amazon page')
 def open_amazon(context):
-    context.driver.get('https://www.amazon.com/')
-    sleep(1)
+    #context.driver.get('https://www.amazon.com/')
+    context.app.main_page.open_main()
 
 
 @when('Click on Orders button')
 def click_orders_button(context):
-    context.driver.find_element(By.XPATH,"//span[text()='& Orders' and @class='nav-line-2']").click()
-    sleep(1)
+    #context.driver.find_element(By.XPATH,"//span[text()='& Orders' and @class='nav-line-2']").click()
+    context.app.header.click_orders()
 
 
-@then('Text Sing In is shown')
+@then('Page Sing In is open')
 def verify_sign_in(context):
-    context.driver.find_element(By.XPATH, "//h1[@class='a-spacing-small']")
-
-
+    #context.driver.find_element(By.XPATH, "//h1[@class='a-spacing-small']")
+    context.app.main_page.verify_page_url("https://www.amazon.com/ap/signin")
 
 
